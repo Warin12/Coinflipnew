bbd9d1c6a18e55f972c6fffe9657b8fac038eee7
Warin12 <evanw8@learn.cssd.ab.ca>
2019-02-27 15:08:17 -0700

    first commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,21 @@ namespace coinflip
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world");
+           int coin;
+           string guess;
+           Random rng = new Random();
+           
+           Console.WriteLine("Guess Heads or tails(H or T)");
+           guess = Console.ReadLine();
+           coin = rng.Next(0,2);
+           if(coin == 0 && guess == "T");
+           {
+               Console.WriteLine("It landed on tails, you win");
+           }
+           else if (coin == 1 && guess == "H");
+           {
+               Console.WriteLine("It was Heads, you win");
+           }
 
         }
     }
