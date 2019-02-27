4addba44f7dd88d896339d2313df6f44823dc817
Warin12 <evanw8@learn.cssd.ab.ca>
2019-02-27 14:48:55 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace coinflip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+
+        }
+    }
+}
