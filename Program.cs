8e0d56af8d18bff30288f36de901f1674498a4d5
Warin12 <evanw8@learn.cssd.ab.ca>
2019-02-28 14:10:54 -0700

    Finished coinflip

--- Program.cs
+++ Program.cs
@@ -13,15 +13,28 @@ namespace coinflip
            Console.WriteLine("Guess Heads or tails(H or T)");
            guess = Console.ReadLine();
            coin = rng.Next(0,2);
-           if(coin == 0 && guess == "T");
+           if(coin == 0 && guess == "T")
            {
                Console.WriteLine("It landed on tails, you win");
            }
-           else if (coin == 1 && guess == "H");
+           else if (coin == 1 && guess == "H")
            {
                Console.WriteLine("It was Heads, you win");
            }
-
+           else
+           {
+               if(coin == 0)
+               {
+               Console.WriteLine("The coin flip was tails, you lose.");
+               }
+               else
+               {
+               Console.WriteLine("The coinflip was Heads, you lose.");
+               }
+           
+           
+           }
+        Console.ReadLine();
         }
     }
 }
